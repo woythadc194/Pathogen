944cd480eefe3ea639d9c028d93716069c4673ea
Dylan Woythal <woythadc194@potsdam.edu>
2013-10-23 23:28:15 -0400

    Added clicking. Only prints to terminal at this point

--- Pathogen.java
+++ Pathogen.java
@@ -10,12 +10,14 @@ public class Pathogen extends JFrame{
     private static int boardWidth;
     private static int boardHeight;
     private static int currentSelectedVal;
-
+    private static ArrayList<ArrayList<JButton>> buttonList;
+    
     public static void main(String[]args){
         try{ numCells = Integer.parseInt( args[0] ); } catch ( Exception e ){}
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new Pathogen().createAndDisplayGUI();
+                gameFunct();
             }
         });
     }
@@ -35,22 +37,35 @@ public class Pathogen extends JFrame{
         buttonPanel.setLayout( new GridLayout( numCells, numCells, 1, 1 ) );
         
         for( int x=0; x<numCells*2; x++ ){
+            ArrayList<JButton> list = new ArrayList<JButton>();
             for( int y=0; y<numCells; y++ ){
-                ImageIcon icon = new ImageIcon( "eCell.jpg" );
+                /*ImageIcon icon = new ImageIcon( "eCell.jpg" );
                 Image img = icon.getImage() ;  
                 Image newimg = img.getScaledInstance( buttonSize, buttonSize,  java.awt.Image.SCALE_FAST ) ;  
-                icon = new ImageIcon( newimg );
-                JButton button = new JButton( icon );
-//                GameButton button = new GameButton( 0, x, y );
+                icon = new ImageIcon( newimg );                */
+                JButton button = new JButton();
+                button.setBackground( Color.black );
+                button.setPreferredSize( new Dimension( buttonSize, buttonSize ) );
                 button.setBorder( BorderFactory.createEmptyBorder() );
+                button.addActionListener(new ActionListener() {
+ 
+                    public void actionPerformed(ActionEvent e){
+                        //Execute when button is pressed
+                        System.out.println("Hello");
+                        //button.setBackground( Color.black );
+                    }
+                });   
                 buttonPanel.add( button );
             }
         }
         contentPane.add( buttonPanel );
         setContentPane( contentPane );
         pack();
-        setLocationByPlatform(true);
+        setLocationByPlatform( true );
         setVisible( true );
+    }
+    
+    private static void gameFunct(){
 
     }
 }
